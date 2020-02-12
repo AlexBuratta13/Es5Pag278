7e0587a3fc8f17d84d4b70d9ea5d8b72d146884a
studenti <studenti@rosselli.loc>
2020-02-12 12:56:51 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿
+using System;
+using System.Collections.Generic;
+
+namespace Es5Pag278
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("quanti biglietti vuoi aquistare");
+            int biglietti = int.Parse(Console.ReadLine());
+            Random r = new Random();
+            int[] arrayBiglietti = new int[biglietti];
+            for (int i = 0; i < arrayBiglietti.Length; i++)
+            {
+                arrayBiglietti[i] = r.Next(10000, 999999);
+                Console.WriteLine($"{arrayBiglietti[i]}");
+            }
+            Console.ReadLine();
+        }
+    }
+}
